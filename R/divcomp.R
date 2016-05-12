0f3a79362b795d6ebf423b7bc1358bb4fdd6af60
Jon Lefcheck <jslefche@vims.edu>
2016-05-12 10:02:46 -0400

    Initial commit

---
+++ R/divcomp.R
@@ -0,0 +1,69 @@
+divcomp = function(mat, dissim = NULL, q = 0) {
+  
+  # Relativize abundance matrix
+  p = as.matrix(apply(mat, 1, function(x) x/sum(x, na.rm = TRUE)))
+  
+  p[, apply(p, 2, function(x) all(is.na(x)))] = 0
+  
+  # Create taxonomic dissimilarity matrix if dissim is empty
+  if(is.null(dissim)) {
+    
+    dissim = matrix(1, nrow = nrow(p), ncol = nrow(p))
+    
+    diag(dissim) = 0
+    
+  } else {
+    
+    dissim = as.matrix(dissim)
+    
+  }
+  
+  if(ncol(mat) != nrow(dissim)) stop("Number of rows must match between mat and groups")
+  
+  # Get similarity matrix from dissimilarity matrix
+  Z = 1 - dissim
+
+  # Calculate expected (average) similarity matrix and store in a matrix Zp
+  Zp = matrix(0, nrow(p), ncol(p))
+  
+  for(k in 1:ncol(p)) { 
+    
+    for(i in 1:nrow(p)) { 
+      
+      for(j in 1:nrow(p)) { 
+        
+        Zp[i, k] = Zp[i, k] + Z[i, j] * p[j, k]
+        
+      } } } 
+
+  # Compute diversity for each sample
+  ret = rep(0, ncol(p))
+  
+  for(k in 1:length(ret)) {
+    
+    if(q == 1) {
+      
+      s = rep(0, nrow(Zp)) 
+      
+      for(i in 1:nrow(Zp)) {
+        
+        s[i] = Zp[i, k] ^ p[i, k]
+        
+      }
+      
+      ret[k] = 1/prod(s)
+      
+    } else {
+      
+      ret[k] = sum(p[, k] * (Zp[, k] ^ (q - 1)), na.rm = TRUE) ^ (1 / (1 - q))
+      
+    }
+    
+  }
+  
+  names(ret) = rownames(mat)
+  
+  # Return results
+  return(ret)
+  
+}
---
+++ R/divpart.R
@@ -0,0 +1,83 @@
+divpart = function(mat, groups, dissim = NULL, q = 0) {
+  
+  if(nrow(mat) != nrow(groups)) stop("Number of rows must match between mat and groups")
+  
+  # Organize groups from most levels to fewest
+  groups. = sapply(colnames(groups), function(x) length(unique(groups[, x])))
+  
+  groups = groups[, names(rev(sort(groups.)))]
+  
+  # Split groups into a list
+  groups.l = lapply(apply(groups, 2, list), unlist)
+  
+  # Split each grouping level and apply analysis
+  do.call(rbind, lapply(length(groups.l):2, function(i) {
+      
+    # Calculate local diversity for each group in the level below the current group
+    alpha = if(i - 1 == 1) {
+        
+        divcomp(mat, dissim = dissim, q = q)
+        
+      } else {
+        
+        sapply(unique(groups.l[[i - 1]]), function(j) {
+      
+          # Subset data for group
+          mat. = mat[j == groups.l[[i - 1]], ]
+          
+          # Summarize at the group level
+          mat. = t(as.matrix(colSums(mat.)))
+            
+          # Calculate local diversity
+          divcomp(mat., dissim = dissim, q = q) 
+          
+        } )
+        
+        }
+      
+    # Get mean alpha diversity
+    mean.alpha = mean(alpha, na.rm = TRUE)
+      
+    # Calculate gamma diversity across groups for the current level
+    gamma = if(i - 1 == 1) {
+      
+      mat. =  t(as.matrix(colSums(mat)))
+      
+      divcomp(mat., dissim = dissim, q = q)
+      
+    } else {
+      
+      sapply(unique(groups.l[[i]]), function(j) {
+      
+      # Subset data for group
+      mat. = mat[j == groups.l[[i]], ]
+      
+      # Summarize at the group level
+      mat. = t(as.matrix(colSums(mat.)))
+      
+      # Calculate local diversity
+      divcomp(mat., dissim = dissim, q = q) 
+      
+      } )
+      
+    }
+    
+    # Get additive beta diversity
+    beta = gamma - mean.alpha
+    
+    # Return in data.frame
+    dat = data.frame(
+      level = i,
+      name = colnames(groups)[i - 1],
+      alpha = mean.alpha,
+      gamma = gamma,
+      beta = beta
+    )
+    
+    rownames(dat) = NULL
+    
+    return(dat)
+    
+  } ) )
+  
+}
